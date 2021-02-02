54f70a8904b0b456e68a6e4b8d6c6a8a6b705db9
Cheryl <cheryljfowlie@me.com>
2021-02-02 20:44:42 +1100

    :building_construction: Update Nav and Page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 // import api from "./utils/API"
-import { BrowserRouter as Router } from "react-router-dom";
-import Nav from "./components/Nav";
-import List from "./components/List"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <List />
+        <Navigation />
+
+
       </div>
     </Router>
   );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,48 @@
-import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap'
+import ListStandUp from '../../pages/standup';
+import ListRelease from '../../pages/release';
 
-
-function Nav() {
+function Navigation() {
   return (
-    <Container>
-  <Navbar fixed="top" expand="lg" variant="light" bg="light">
-    <Navbar.Brand href="#">Navbar</Navbar.Brand>
-  </Navbar>
-</Container>
-  );
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <Router>
+            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+              <Navbar.Brand href="#home">Test Dashoard</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Tests</Nav.Link>
+                  <Nav.Link href="/standup">Stan Up</Nav.Link>
+                  <Nav.Link href="/release">Releases</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <br />
+            <Switch>
+              <Route exact path="/">
+                <h1>Testing</h1>
+              </Route>
+              <Route path="/standup">
+                <ListStandUp />
+              </Route>
+              <Route path="/release">
+                <ListRelease />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Nav;
+
+export default Navigation;
