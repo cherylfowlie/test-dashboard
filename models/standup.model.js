685b1f726dab34d7c1257df9615c25315027fd0a
Cheryl <cheryljfowlie@me.com>
2021-02-10 00:04:47 +1100

    Update models

---
+++ models/standup.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const standupSchema = new Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    platform: { type: String, required: true },
+    date: { type: Date, required: true },
+}, {
+    timestamps: true,
+});
+
+const Standup = mongoose.model('Standup', standupSchema);
+
+module.exports = Standup;
