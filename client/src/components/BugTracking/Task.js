6d67650b8327a86c5cca9480b5f86b4148505a87
Cheryl <cheryljfowlie@me.com>
2021-02-17 20:52:59 +1100

    Add Presentation .zip File

--- client/src/components/BugTracking/Task.js
+++ client/src/components/BugTracking/Task.js
@@ -1,148 +1,147 @@
 import "../styles/task.scss";
 import { useState } from "react";
 import React from "react";
-
 export default function Task(props) {
-  const { addBug, deleteBug, moveBug, task } = props;
+    const { addTask, deleteTask, moveTask, task } = props;
 
-  const [urgencyLevel, setUrgencyLevel] = useState(task.urgency);
-  const [collapsed, setCollapsed] = useState(task.isCollapsed);
-  const [formAction, setFormAction] = useState("");
+    const [urgencyLevel, setUrgencyLevel] = useState(task.urgency);
+    const [collapsed, setCollapsed] = useState(task.isCollapsed);
+    const [formAction, setFormAction] = useState("");
 
-  function setUrgency(event) {
-    setUrgencyLevel(event.target.attributes.urgency.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    if (formAction === "save") {
-      if (collapsed) {
-        setCollapsed(false);
-      } else {
-        let newTask = {
-          id: task.id,
-          title: event.target.elements.title.value,
-          description: event.target.elements.description.value,
-          urgency: urgencyLevel,
-          status: task.status,
-          isCollapsed: true,
-        };
-
-        addBug(newTask);
-        setCollapsed(true);
-      }
+    function setUrgency(event) {
+        setUrgencyLevel(event.target.attributes.urgency.value);
     }
 
-    if (formAction === "delete") {
-      deleteBug(task.id);
-    }
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
 
-  function handleMoveLeft() {
-    let newStatus = "";
+        if (formAction === "save") {
+            if (collapsed) {
+                setCollapsed(false);
+            } else {
+                let newTask = {
+                    id: task.id,
+                    title: event.target.elements.title.value,
+                    description: event.target.elements.description.value,
+                    urgency: urgencyLevel,
+                    status: task.status,
+                    isCollapsed: true
+                };
 
-    if (task.status === "In Development") {
-      newStatus = "Bug List";
-    } else if (task.status === "QA") {
-      newStatus = "In Development";
-    }
+                addTask(newTask);
+                setCollapsed(true);
+            }
+        }
 
-    if (newStatus !== "") {
-      moveBug(task.id, newStatus);
-    }
-  }
-
-  function handleMoveRight() {
-    let newStatus = "";
-
-    if (task.status === "Bug List") {
-      newStatus = "In Development";
-    } else if (task.status === "In Development") {
-      newStatus = "QA";
+        if (formAction === "delete") {
+            deleteTask(task.id);
+        }
     }
 
-    if (newStatus !== "") {
-      moveBug(task.id, newStatus);
-    }
-  }
+    function handleMoveLeft() {
+        let newStatus = "";
 
-  return (
-    <div className={`task ${collapsed ? "collapsedTask" : ""}`}>
-      <button onClick={handleMoveLeft} className="button moveBug">
-        &#171;
+        if (task.status === "In Development 👩‍💻") {
+            newStatus = "Bug Raised 🐛";
+        } else if (task.status === "QA ✅") {
+            newStatus = "In Development 👩‍💻";
+        }
+
+        if (newStatus !== "") {
+            moveTask(task.id, newStatus);
+        }
+    }
+
+    function handleMoveRight() {
+        let newStatus = "";
+
+        if (task.status === "Bug Raised 🐛") {
+            newStatus = "In Development 👩‍💻";
+        } else if (task.status === "In Development 👩‍💻") {
+            newStatus = "QA ✅";
+        }
+
+        if (newStatus !== "") {
+            moveTask(task.id, newStatus);
+        }
+    }
+
+    return (
+        <div className={`task ${collapsed ? "collapsedTask" : ""}`}>
+            <button onClick={handleMoveLeft} className="button moveTask">
+                &#171;
       </button>
-      <form onSubmit={handleSubmit} className={collapsed ? "collapsed" : ""}>
-        <input
-          type="text"
-          className="title input"
-          name="title"
-          placeholder="Enter Title"
-          disabled={collapsed}
-          defaultValue={task.title}
-        />
-        <textarea
-          rows="2"
-          className="description input"
-          name="description"
-          placeholder="Enter Description"
-          defaultValue={task.description}
-        />
-        <div className="urgencyLabels">
-          <label className={`low ${urgencyLevel === "low" ? "selected" : ""}`}>
-            <input
-              urgency="low"
-              onChange={setUrgency}
-              type="radio"
-              name="urgency"
-            />
+            <form onSubmit={handleSubmit} className={collapsed ? "collapsed" : ""}>
+                <input
+                    type="text"
+                    className="title input"
+                    name="title"
+                    placeholder="Enter Title"
+                    disabled={collapsed}
+                    defaultValue={task.title}
+                />
+                <textarea
+                    rows="2"
+                    className="description input"
+                    name="description"
+                    placeholder="Enter Description"
+                    defaultValue={task.description}
+                />
+                <div className="urgencyLabels">
+                    <label className={`low ${urgencyLevel === "low" ? "selected" : ""}`}>
+                        <input
+                            urgency="low"
+                            onChange={setUrgency}
+                            type="radio"
+                            name="urgency"
+                        />
             low
           </label>
-          <label
-            className={`medium ${urgencyLevel === "medium" ? "selected" : ""}`}
-          >
-            <input
-              urgency="medium"
-              onChange={setUrgency}
-              type="radio"
-              name="urgency"
-            />
+                    <label
+                        className={`medium ${urgencyLevel === "medium" ? "selected" : ""}`}
+                    >
+                        <input
+                            urgency="medium"
+                            onChange={setUrgency}
+                            type="radio"
+                            name="urgency"
+                        />
             medium
           </label>
-          <label
-            className={`high ${urgencyLevel === "high" ? "selected" : ""}`}
-          >
-            <input
-              urgency="high"
-              onChange={setUrgency}
-              type="radio"
-              name="urgency"
-            />
+                    <label
+                        className={`high ${urgencyLevel === "high" ? "selected" : ""}`}
+                    >
+                        <input
+                            urgency="high"
+                            onChange={setUrgency}
+                            type="radio"
+                            name="urgency"
+                        />
             high
           </label>
-        </div>
-        <button
-          onClick={() => {
-            setFormAction("save");
-          }}
-          className="button"
-        >
-          {collapsed ? "Edit" : "Save"}
-        </button>
-        {collapsed && (
-          <button
-            onClick={() => {
-              setFormAction("delete");
-            }}
-            className="button delete"
-          >
-            X
-          </button>
-        )}
-      </form>
-      <button onClick={handleMoveRight} className="button moveBug">
-        &#187;
+                </div>
+                <button
+                    onClick={() => {
+                        setFormAction("save");
+                    }}
+                    className="button"
+                >
+                    {collapsed ? "Edit" : "Save"}
+                </button>
+                {collapsed && (
+                    <button
+                        onClick={() => {
+                            setFormAction("delete");
+                        }}
+                        className="button delete"
+                    >
+                        X
+                    </button>
+                )}
+            </form>
+            <button onClick={handleMoveRight} className="button moveTask">
+                &#187;
       </button>
-    </div>
-  );
+        </div>
+    );
 }
